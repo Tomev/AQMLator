e201a8f6121a0ca434a6f3f1d53ef3280849b2b4
Tomev <rybotycki.tomasz+github@gmail.com>
2025-03-15 12:21:58 +0100

    feat(version): Version 0.2.0 of AQMLator.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="aqmlator",
-    version="0.1.2",
+    version="0.2.0",
     author="Tomasz Rybotycki",
     author_email="rybotycki.tomasz+aqmlator@gmail.com",
     long_description=long_description,
