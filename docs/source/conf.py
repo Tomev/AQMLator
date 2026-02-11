d8628e8163402be7a3ba18e65d1483421bc924ee
Tomasz Rybotycki <rybotycki.tomasz+git@gmail.com>
2026-02-11 23:17:01 +0100

    bump(version): Update version to 1.1.0.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2023, Tomasz Rybotycki"
 author = "Tomasz Rybotycki"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "1.1.0"
 
 
 # -- General configuration ---------------------------------------------------
